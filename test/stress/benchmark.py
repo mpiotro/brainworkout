8db3f0c2e3b677150bdf8d456f49ef9dffd9d3a8
michal <mpiotrow.box@gmail.com>
2021-05-09 20:33:29 +0200

    Add simple sort benchmark

---
+++ test/stress/benchmark.py
@@ -0,0 +1,25 @@
+import time
+from random import randint
+
+from src.algorithms.search.bubblesort import bubble_sort
+from src.algorithms.search.mergesort import merge_sort
+
+if __name__ == "__main__":
+
+    values = [randint(-10000, 10000) for value in range(1, 100000)]
+
+    tic = time.perf_counter()
+
+    bubble_sort(values)
+
+    toc = time.perf_counter()
+
+    print(f"Bubble sort finished in {toc - tic:0.4f} seconds")
+
+    tic = time.perf_counter()
+
+    merge_sort(values)
+
+    toc = time.perf_counter()
+
+    print(f"Merge sort finished in {toc - tic:0.4f} seconds")
