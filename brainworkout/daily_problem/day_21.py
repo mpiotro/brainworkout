2048deab024507f95b33ff8e47286346f2f00757
Michal Piotrowski <micpi@equinor.com>
2023-12-10 15:34:00 +0100

    Add solution for day 21.

--- brainworkout/daily_problem/day_21.py
+++ brainworkout/daily_problem/day_21.py
@@ -4,15 +4,38 @@
 
 
 def solution(timetable: list):
+
+    if len(timetable) < 2:
+        return len(timetable)
+
+    my_dict = {}
+    for start, end in timetable:
+        if start in my_dict:
+            my_dict[start] += 1
+        else:
+            my_dict[start] = 1
+
+        if end in my_dict:
+            my_dict[end] -= 1
+        else:
+            my_dict[end] = -1
+
+    keys = sorted(my_dict.keys())
+
     classrooms = 0
-    timetable = sorted(timetable)
-    return classrooms
+    max_classrooms = 0
+    for k in keys:
+        classrooms += my_dict[k]
+        if classrooms > max_classrooms:
+            max_classrooms = classrooms
+
+    return max_classrooms
 
 
 if __name__ == "__main__":
     assert solution([]) == 0
     assert solution([(0, 50)]) == 1
-    assert solution([(0, 50), (60, 150)]) == 2
+    assert solution([(0, 50), (60, 150)]) == 1
     assert solution([(30, 75), (0, 50), (60, 150)]) == 2
     assert solution([(30, 75), (0, 50), (60, 150), (0, 50)]) == 3
-    assert solution([(30, 75), (75, 150), (60, 140), (120, 180), (10, 20)]) == 4
+    assert solution([(30, 75), (75, 150), (60, 140), (120, 180), (10, 20)]) == 3
